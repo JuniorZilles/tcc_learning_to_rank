12758513f3f568fc382af6f40d7ce426d6c037b2
Junior Zilles <junior_zilles@hotmail.com>
2021-11-30 15:10:50 -0300

    fix: save folder

--- src/parameter_tuning.py
+++ src/parameter_tuning.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from convert import toDataframe
 from flaml import AutoML
+
 for data in ['MSLR10K', 'MSLR30K', 'OHSUMED', 'TD2003', 'TD2004']:
     path = Path(__file__).absolute().parents[1] / 'data' / data
     group_train, y_train, X_train = toDataframe(str(path/'train.txt'))
@@ -9,11 +10,11 @@ for data in ['MSLR10K', 'MSLR30K', 'OHSUMED', 'TD2003', 'TD2004']:
         for boost in ['lgbm', 'xgboost']:
                 automl = AutoML()
                 automl.fit(
-                    X_train, y_train, groups=group_train, log_file_name=f'{task}.{boost}.{data}.flaml.log', 
-                    task=task, time_budget=1000,  estimator_list=[boost], 
+                    X_train, y_train, groups=group_train, log_file_name=f'flaml_logs/{task}.{boost}.{data}.flaml.log', 
+                    task=task, time_budget=10000,  estimator_list=[boost], 
                 )
                 # Print the best model
-                with open(f'{task}.{boost}.{data}.flaml.log', 'a') as f:
+                with open(f'flaml_logs/{task}.{boost}.{data}.flaml.log', 'a') as f:
                     f.write(f'Best ML leaner: {automl.best_estimator}')
                     f.write(f'Best hyperparmeter config: {automl.best_config}')
                     f.write(str(automl.model.estimator))
